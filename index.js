5ebd829019a5a9b8f95cf7af555926f96f88f106
Brad Dickason <brad.dickason@oculus.com>
2017-05-21 12:45:58 -0700

    Adjusted module loading mechanism

--- index.js
+++ index.js
@@ -1,8 +1,2 @@
 /* twitch-overlay-text - Text to Speech (TTS) and Display module for twitch-overlay */
-var text = require('./lib/text.js')
-
-module.exports = {
-  start: text.start,
-  overlay: text.overlay,
-  triggers: text.triggers
-}
+module.exports  = require('./lib/text.js')
